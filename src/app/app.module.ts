be25d0bb9f113160488e1c871aeae0ffeec63576
Michael Cross <Michael.Cross@hamiltoncompany.com>
2022-12-16 14:43:38 -0800

    Navigation sidebar imports but they're being hella sus and don't work :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppRoutingModule } from "./Routing/app-routing.module";
+
+// Components
 import { AppComponent } from "./app.component";
 import { CollectionComponent } from "./Components/collection/collection.component";
 import { ProfileComponent } from "./Components/profile/profile.component";
@@ -9,6 +11,18 @@ import { WildComponent } from "./Components/wild/wild.component";
 import { StoreComponent } from './Components/store/store.component';
 import { SettingsComponent } from './Components/settings/settings.component';
 import { GuildComponent } from './Components/guild/guild.component';
+
+// Navigation modules
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { FormsModule } from '@angular/forms';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatDividerModule } from "@angular/material/divider";
+import { MatTabsModule } from '@angular/material/tabs';
 
 @NgModule({
     declarations: [
@@ -20,7 +34,32 @@ import { GuildComponent } from './Components/guild/guild.component';
         SettingsComponent,
         GuildComponent,
     ],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatSidenavModule,
+        MatButtonModule,
+        MatListModule,
+        FormsModule,
+        MatCheckboxModule,
+        MatTreeModule,
+        MatProgressBarModule,
+        MatToolbarModule,
+        MatDividerModule,
+        MatTabsModule,
+    ],
+    exports: [
+        MatSidenavModule,
+        MatButtonModule,
+        MatListModule,
+        FormsModule,
+        MatCheckboxModule,
+        MatTreeModule,
+        MatProgressBarModule,
+        MatToolbarModule,
+        MatDividerModule,
+        MatTabsModule,
+    ],
     providers: [],
     bootstrap: [AppComponent],
 })
