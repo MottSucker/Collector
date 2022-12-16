fb84974fbeca1a66e26e5b0da0f1505e222db087
Centipede Enthusiast <emerald5129@rocketmail.com>
2022-12-15 18:36:22 -0800

    these line ending making me MAD

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CollectionComponent } from './collection/collection.component';
+import { ProfileComponent } from './Modules/profile/profile.component';
+import { WildComponent } from './Modules/wild/wild.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CollectionComponent,
+    ProfileComponent,
+    WildComponent
   ],
   imports: [
     BrowserModule,
