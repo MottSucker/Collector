ce7a15170a09761631360331ba77dfa4f93ec6c2
Centipede Enthusiast <emerald5129@rocketmail.com>
2022-12-16 15:27:12 -0800

    somewhat sus

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,21 +8,21 @@ import { AppComponent } from "./app.component";
 import { CollectionComponent } from "./Components/collection/collection.component";
 import { ProfileComponent } from "./Components/profile/profile.component";
 import { WildComponent } from "./Components/wild/wild.component";
-import { StoreComponent } from './Components/store/store.component';
-import { SettingsComponent } from './Components/settings/settings.component';
-import { GuildComponent } from './Components/guild/guild.component';
+import { StoreComponent } from "./Components/store/store.component";
+import { SettingsComponent } from "./Components/settings/settings.component";
+import { GuildComponent } from "./Components/guild/guild.component";
 
 // Navigation modules
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { FormsModule } from '@angular/forms';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
-import { MatTreeModule } from '@angular/material/tree';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { FormsModule } from "@angular/forms";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatButtonModule } from "@angular/material/button";
+import { MatListModule } from "@angular/material/list";
+import { MatTreeModule } from "@angular/material/tree";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatToolbarModule } from "@angular/material/toolbar";
 import { MatDividerModule } from "@angular/material/divider";
-import { MatTabsModule } from '@angular/material/tabs';
+import { MatTabsModule } from "@angular/material/tabs";
 
 @NgModule({
     declarations: [
